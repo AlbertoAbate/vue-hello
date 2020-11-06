0d3ae0149124dacaf68328ce5e70e5dedd9e6f9c
Alberto <alberto.abate87@gmail.com>
2020-11-06 16:08:40 +0100

    esercizio completo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+  el:"#app",
+  data: {
+    title:"Hello world!!!",
+    message:"Benvenuti nel mondo di Vue!!",
+    image:"http://www.fillmurray.com/200/300",
+    titolo:"gosthbuster"
+  }
+});
